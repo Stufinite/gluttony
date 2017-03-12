bdfed2a35f368a3524ec59eebcb1cb604d5c1a05
davidtnfsh <davidtnfsh@gmail.com>
2017-03-12 15:28:27 +0800

    update setup.py, the requirements shouldn't assigne version num

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name = 'gluttonyTw',
     packages = ['gluttonyTw', 'gluttonyTw/view'],
-    version = '2.0',
+    version = '2.1',
     description = 'An API for time2eat.',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/Stufinite/gluttonyTw',
-    download_url = 'https://github.com/Stufinite/gluttony/archive/v2.0.tar.gz',
+    download_url = 'https://github.com/Stufinite/gluttony/archive/v2.1.tar.gz',
     keywords = ['time2eat', 'campass'],
     classifiers = [],
     license='GPL3.0',
     install_requires=[
-        'djangoApiDec==1.2',
+        'djangoApiDec',
     ],
     zip_safe=True,
 )
